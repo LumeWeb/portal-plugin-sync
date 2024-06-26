df34a2796f7a0884fe761c4a59cf6d66239c9f62
Derrick Hammer <derrick@derrickhammer.com>
2024-06-26 18:35:11 -0400

    fix: sync bucket name should be lower case

--- internal/cron/tasks/tasks.go
+++ internal/cron/tasks/tasks.go
@@ -16,7 +16,7 @@ import (
 	"io"
 )
 
-const syncBucketName = "Sync"
+const syncBucketName = "sync"
 
 func getSyncProtocol(protocol string) (types.SyncProtocol, error) {
 	proto := core.GetProtocol(protocol)
